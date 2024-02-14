6d50d19bc0ece0897cbd090155c408a5fa1ddb25
donCelis <alexcelis019@gmail.com>
2024-02-13 22:23:12 -0500

    Add dynamic table rows

--- db-alumnos/src/main.js
+++ db-alumnos/src/main.js
@@ -1,12 +1,25 @@
 import { thC, titles } from "./components/th";
+import { trC } from "./components/tr";
+import { jedis } from "./db";
 import "./styles/app.css";
 
 // DOM
 const $tableContainer = document.querySelector("#table-container");
 const $thMain = $tableContainer.querySelector("thead tr");
+const $tbody = $tableContainer.querySelector("tbody");
+
+const updateList = (array = jedis) => {
+  $tbody.innerHTML = "";
+
+  array.forEach((element) => {
+    $tbody.innerHTML += trC({ ...element });
+  });
+}
 
 window.addEventListener("DOMContentLoaded", (e) => {
   titles.forEach((title) => {
     $thMain.innerHTML += thC({ title });
   });
+
+  updateList();
 });
