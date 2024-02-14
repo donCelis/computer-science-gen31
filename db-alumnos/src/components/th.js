c894b4b081a6ba9e741b7b4bb4f17dc5b426d869
donCelis <alexcelis019@gmail.com>
2024-02-13 21:49:03 -0500

    Add table header component

---
+++ db-alumnos/src/components/th.js
@@ -0,0 +1,5 @@
+export const titles = ["Name", "State", "Role", "Team", ""];
+
+export const thC = ({ title }) => {
+  return `<th scope="col" class="px-6 py-4 font-medium text-gray-900">${title}</th>`;
+};
