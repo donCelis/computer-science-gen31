259094ea3e241c770e03a216d1882f848adea01f
donCelis <alexcelis019@gmail.com>
2024-02-13 20:54:06 -0500

    Add Jedi database to project

---
+++ db-alumnos/src/db.js
@@ -0,0 +1,92 @@
+export const jedis = [
+  {
+    name: "Luke Skywalker",
+    age: 30,
+    team: "Rebel Alliance",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Obi-Wan Kenobi",
+    age: 60,
+    team: "Jedi Order",
+    role: "Maestro de la Orden",
+  },
+  {
+    name: "Yoda",
+    age: 900,
+    team: "Jedi Order",
+    role: "Maestro de la Orden",
+  },
+  {
+    name: "Anakin Skywalker",
+    age: 23,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Mace Windu",
+    age: 50,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Ahsoka Tano",
+    age: 18,
+    team: "Jedi Order",
+    role: "Padawan",
+  },
+  {
+    name: "Qui-Gon Jinn",
+    age: 40,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Kit Fisto",
+    age: 30,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Ezra Bridger",
+    age: 20,
+    team: "Rebel Alliance",
+    role: "Padawan",
+  },
+  {
+    name: "Plo Koon",
+    age: 45,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Rey",
+    age: 25,
+    team: "Resistance",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Kylo Ren",
+    age: 30,
+    team: "First Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Aayla Secura",
+    age: 35,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Kanan Jarrus",
+    age: 32,
+    team: "Rebel Alliance",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Barriss Offee",
+    age: 28,
+    team: "Jedi Order",
+    role: "Padawan",
+  },
+];
