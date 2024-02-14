bdbf4ef85847db5c190e90bbe13386f4395d7545
donCelis <alexcelis019@gmail.com>
2024-02-13 21:49:23 -0500

    Add dynamic table headers

--- db-alumnos/src/main.js
+++ db-alumnos/src/main.js
@@ -1,2 +1,12 @@
-import './styles/app.css'
+import { thC, titles } from "./components/th";
+import "./styles/app.css";
 
+// DOM
+const $tableContainer = document.querySelector("#table-container");
+const $thMain = $tableContainer.querySelector("thead tr");
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  titles.forEach((title) => {
+    $thMain.innerHTML += thC({ title });
+  });
+});
