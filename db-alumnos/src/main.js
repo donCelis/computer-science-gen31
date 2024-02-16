22c7cd571037f7f0fe6875726a3ec719b736626e
donCelis <alexcelis019@gmail.com>
2024-02-16 12:57:39 -0500

    Add form functionality and create user feature

--- db-alumnos/src/main.js
+++ db-alumnos/src/main.js
@@ -1,9 +1,11 @@
+import { formC } from "./components/form";
 import { thC } from "./components/th";
 import { trC } from "./components/tr";
-import { jedis, titles } from "./db";
+import { jedis, roleOptions, teamOptions, titles } from "./db";
 import "./styles/app.css";
+import { getFormValues } from "./utils/getFormValues";
 
-//TODO: crear la funcionalidad para la modal
+//TODO: crear la funcionalidad para la modal -> ok
 //TODO: agregar usuarios
 //TODO: editar usuarios
 //TODO: borrar usuarios
@@ -16,6 +18,7 @@ const $thMain = $tableContainer.querySelector("#tr");
 const $tbody = $tableContainer.querySelector("tbody");
 
 const $modal = document.querySelector("#modal");
+const $modalContainer = $modal.querySelector("#modal-container");
 const $openModal = document.querySelector("#open-modal");
 const $closeModal = document.querySelector("#close-modal");
 
@@ -28,12 +31,38 @@ const updateList = (array = jedis) => {
 };
 
 window.addEventListener("DOMContentLoaded", (e) => {
+  // modal
   $openModal.addEventListener("click", () => $modal.classList.remove("hidden"));
   $closeModal.addEventListener("click", () => $modal.classList.add("hidden"));
+  $modalContainer.innerHTML += formC({ id: "create-user" });
 
+  // titles table
   titles.forEach((title) => {
     $thMain.innerHTML += thC({ title });
   });
 
+  // render list users
   updateList();
+
+  // create user
+  const $form = document.querySelector("#create-user");
+  $form.addEventListener("submit", handleSubmit);
 });
+
+// create user
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const tmpUser = getFormValues(e.target);
+  const newUser = {
+    ...tmpUser,
+    role: roleOptions[tmpUser.role],
+    team: teamOptions[tmpUser.team],
+  };
+
+  jedis.push(newUser);
+  updateList();
+  
+  e.target.reset();
+  $modal.classList.add("hidden");
+};
