206788526127ae319e1daa61c15881e8c7ded0dd
donCelis <alexcelis019@gmail.com>
2024-02-15 20:38:00 -0500

    Add avatar component

---
+++ db-alumnos/src/components/avatar.js
@@ -0,0 +1,8 @@
+import { getLetters } from "../utils/getLetters";
+
+export const avatarC = ({ name }) => {
+  const generateName = getLetters(name);
+  return `<p class="inline-flex justify-center items-center bg-sky-600 text-white h-full w-full rounded-full">
+    ${generateName}
+  </p>`;
+};
