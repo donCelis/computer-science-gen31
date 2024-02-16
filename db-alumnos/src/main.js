d6540bff0e181bd9e64ca2d4db619fbe6214c812
donCelis <alexcelis019@gmail.com>
2024-02-15 22:10:07 -0500

    Add modal functionality and user management

--- db-alumnos/src/main.js
+++ db-alumnos/src/main.js
@@ -3,10 +3,21 @@ import { trC } from "./components/tr";
 import { jedis, titles } from "./db";
 import "./styles/app.css";
 
+//TODO: crear la funcionalidad para la modal
+//TODO: agregar usuarios
+//TODO: editar usuarios
+//TODO: borrar usuarios
+//TODO: buscar usuarios
+//TODO: ordenar usuarios
+
 // DOM
 const $tableContainer = document.querySelector("#table-container");
 const $thMain = $tableContainer.querySelector("#tr");
 const $tbody = $tableContainer.querySelector("tbody");
+
+const $modal = document.querySelector("#modal");
+const $openModal = document.querySelector("#open-modal");
+const $closeModal = document.querySelector("#close-modal");
 
 const updateList = (array = jedis) => {
   $tbody.innerHTML = "";
@@ -17,6 +28,9 @@ const updateList = (array = jedis) => {
 };
 
 window.addEventListener("DOMContentLoaded", (e) => {
+  $openModal.addEventListener("click", () => $modal.classList.remove("hidden"));
+  $closeModal.addEventListener("click", () => $modal.classList.add("hidden"));
+
   titles.forEach((title) => {
     $thMain.innerHTML += thC({ title });
   });
