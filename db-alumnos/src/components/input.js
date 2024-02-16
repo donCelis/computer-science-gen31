0faaa61afce21f2993fa117ff275406cd987d23d
donCelis <alexcelis019@gmail.com>
2024-02-16 12:57:26 -0500

    Add inputC component to handle input fields

---
+++ db-alumnos/src/components/input.js
@@ -0,0 +1,19 @@
+export const inputC = ({ type, name, placeholder, required = false }) => {
+  return `
+    <div>
+      <label
+        for="${name}"
+        class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+        >Name</label
+      >
+      <input
+        id="${name}"
+        type="${type}"
+        name="${name}"
+        class="text-gray-600 font-normal w-full h-10 pl-3 text-sm rounded-lg border border-gray-300 focus:outline-none focus:border focus:border-blue-500"
+        placeholder="${placeholder}"
+        required="${required}"
+      />
+    </div>
+  `;
+};
