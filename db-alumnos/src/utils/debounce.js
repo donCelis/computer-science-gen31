44c592aa8085f057ab6d1491c073864f34b17a31
donCelis <alexcelis019@gmail.com>
2024-02-21 12:55:04 -0500

    Refactor debounce function

--- db-alumnos/src/utils/debounce.js
+++ db-alumnos/src/utils/debounce.js
@@ -2,12 +2,10 @@ export const debounce = (func, wait) => {
   let timeout;
 
   return (...args) => {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-
     clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
   };
 };
