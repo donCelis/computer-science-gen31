6db75333baa3b47865ed9f4b3925d1f01b9bec60
donCelis <alexcelis019@gmail.com>
2024-02-20 22:32:19 -0500

    Add form functionality for editing user details

--- db-alumnos/src/scripts/edit.js
+++ db-alumnos/src/scripts/edit.js
@@ -1,11 +1,37 @@
-import { jedis } from "../db";
+import { jedis, roleOptions, teamOptions } from "../db";
 import { updateList } from "./read";
+import { $modal, $modalContainer } from "../main";
+import { formC } from "../components/form";
+
+const handleSubmitEdit = (e, index, tempUser) => {
+  e.preventDefault();
+
+  const $form = e.target;
+
+  jedis[index] = {
+    ...tempUser,
+    name: $form.name.value,
+    age: $form.age.value,
+    role: roleOptions[$form.role.value],
+    team: teamOptions[$form.team.value],
+  };
+
+  updateList();
+  $modal.classList.add("hidden");
+  e.target.reset();
+};
 
 export const handleEdit = (event) => {
   const index = event.dataset.index;
   const tempUser = jedis[index];
 
-  console.log(tempUser);
+  $modal.classList.remove("hidden");
+  $modalContainer.innerHTML = formC({ id: "edit-user" });
 
-  updateList();
+  const $form = document.querySelector("#edit-user");
+
+  $form.name.value = tempUser.name;
+  $form.age.value = tempUser.age;
+
+  $form.addEventListener("submit", (e) => handleSubmitEdit(e, index, tempUser));
 };
