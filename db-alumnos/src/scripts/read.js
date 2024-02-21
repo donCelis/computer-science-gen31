786f166451c5448c9bf543c313e8cb37a209d562
donCelis <alexcelis019@gmail.com>
2024-02-20 20:47:25 -0500

    Add handleEdit functionality to updateList

--- db-alumnos/src/scripts/read.js
+++ db-alumnos/src/scripts/read.js
@@ -2,12 +2,18 @@ import { trC } from "../components/tr";
 import { jedis } from "../db";
 import { $tbody } from "../main";
 import { handleDelete } from "./delete";
+import { handleEdit } from "./edit";
 
 export const updateList = (array = jedis) => {
   $tbody.innerHTML = "";
 
   array.forEach((element, index) => {
     $tbody.innerHTML += trC({ ...element, index });
+  });
+
+  const $editButtons = $tbody.querySelectorAll("[data-action='edit']");
+  $editButtons.forEach(($button) => {
+    $button.addEventListener("click", () => handleEdit($button));
   });
 
   const $deleteButtons = $tbody.querySelectorAll("[data-action='delete']");
