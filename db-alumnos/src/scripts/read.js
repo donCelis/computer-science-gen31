1acf6a95e4c55453284559c47558e1498c254f90
donCelis <alexcelis019@gmail.com>
2024-02-20 23:01:29 -0500

    Add conditional rendering for empty array in updateList function

--- db-alumnos/src/scripts/read.js
+++ db-alumnos/src/scripts/read.js
@@ -1,11 +1,20 @@
 import { trC } from "../components/tr";
-import { jedis } from "../db";
+import { jedis, titles } from "../db";
 import { $tbody } from "../main";
 import { handleDelete } from "./delete";
 import { handleEdit } from "./edit";
 
 export const updateList = (array = jedis) => {
   $tbody.innerHTML = "";
+
+  if (!array.length) {
+    $tbody.innerHTML = `
+      <tr>
+        <td colspan="${titles.length}"><p class="text-center py-2">No hay usuarios <p/></td>
+      </tr>
+    `;
+    return;
+  }
 
   array.forEach((element, index) => {
     $tbody.innerHTML += trC({ ...element, index });
